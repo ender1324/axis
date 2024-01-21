b9ebce27fafea62115626c6de474a941b79b709b
ender1324 <ender1324@abv.bg>
2024-01-21 20:24:53 +0200

    initial commit

---
+++ common/defines/be_mod_graphics.lua
@@ -0,0 +1,18 @@
+
+NDefines_Graphics.NGraphics.SUN_LATITUDE = 0
+
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_R = 0.3
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_G = 0.5
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_PROVINCE_B = 1.0
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_R = 0.4
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_G = 0.6
+NDefines_Graphics.NGraphics.BORDER_COLOR_SELECTION_STATE_B = 1.0
+
+NDefines_Graphics.NGraphics.MOON_HEIGHT = 10000 -- higher means softer shadows and more intense light
+NDefines_Graphics.NGraphics.MOON_HEIGHT_WATER = 10000   -- higher means softer shadows and more intense light
+
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 1.0
+NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.95
+
+NDefines_Graphics.NGraphics.MOON_DIFFUSE_COLOR = {0.7, 0.1, 1.0}
+NDefines_Graphics.NGraphics.MOON_INTENSITY = 1.0
