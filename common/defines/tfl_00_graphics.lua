1f3db9a057fb11d21fa7411740da0954d1a80cd3
ender1324 <ender1324@abv.bg>
2024-12-28 17:32:00 +0200

    redo defines overriding for 1.15

---
+++ common/defines/tfl_00_graphics.lua
@@ -0,0 +1,8 @@
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_DEFAULT_COLOR = { 0.43, 0.22, 0.22, 0.25 } 			-- Color of states/provinces that can't be built on
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_MAX_LEVEL_COLOR = { 0.05, 0.1, 0.7, 0.4 } 			-- Color of states/provinces where current building level is maxed out (max is current max level, not final max level) of a building type
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_LEVEL_LOW_COLOR = { 0.05, 0.22, 0.0, 0.4 }
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_LEVEL_HI_COLOR = { 0.4, 0.9, 0.0, 0.5 }
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_MAX_AMOUNT_QUEUED_COLOR = { 0.0, 0.0, 1.0 } 	-- Color of states/provinces when building queue is maxed of a building type
+NDefines_Graphics.NMapMode.CONSTRUCTION_MAP_MODE_BUILDING_QUEUED_COLOR = { 1.0, 0.85, 0.0 }				-- Color of states/provinces when building queue contains one or more of a building type.
+
+NDefines_Graphics.NGraphics.POLITICAL_GRID_SMALL_BOX_LIMIT = 8              	-- Limit for gridbox in political view before it will be replaced with extended gridbox
